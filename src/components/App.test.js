8bfd4b9effeeac46d695073c1de39feaa631a46a
Louise Reid <l_reid@hotmail.co.uk>
2018-05-10 11:43:48 +0100

    action, reducer and wallet component TDD

---
+++ src/components/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import setupTests from './../setupTests.js';
+import tempPolyfills from './../tempPolyfills';
+import App from './App'
+
+describe('App', () => {
+
+  const app = shallow(<App/>);
+
+  it('renders properly', () => {
+    expect(app).toMatchSnapshot()
+  });
+
+  it('contains a connected wallet component', () => {
+    expect(app.find('Connect(Wallet)').exists()).toBe(true)
+  });
+
+})
